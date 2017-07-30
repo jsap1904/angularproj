2fa4ec92518fc86d67042900be5f4fb043a71be5
jsap1904 <jsapdevs@gmail.com>
2017-07-30 13:04:15 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
 var app = angular.module("myApp", []);
 
-app.controller('firstCtrl', function($scope) {
+app.controller('firstCtrl', function($scope, $http) {
 	$scope.myName = 'Jet';
+//take in user input and feed to my api call
+	$scope.searchApi= function(search) {
+	
+	//ny times api
+	$http.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + search + '&api-key=c2bf515d46f3478ab0c62810e26b19ce').then(function(success){
 
-	console.log($scope.myName);
+		//console.log(success.data.response.docs);
+		//store array of objects in my scope variable
+		$scope.articles = success.data.response.docs
+	
+});
+
+
+
+//$scope.searchApi= function(x) {
+//console.log(x);
+//}
 
 //change name function
-	$scope.logChanges= function(x) {
-		console.log(x);
-	};
+	// $scope.logChanges= function(x) {
+	// 	console.log(x);
+	// };
 
-	$scope.nameArr = ['brian', 'nick', 'sam', 'matt'];
+	// $scope.nameArr = ['brian', 'nick', 'sam', 'matt'];
+
+
+	// 	var brian = {
+	// 		location: "SF",
+	// 		age: 2
+	// 	}
+	// 	var nick = {
+	// 		location: "Oakland",
+	// 		age: 1
+	// 	}
+	// 	var sam = {
+	// 		location: "SF",
+	// 		age: 3
+	// 	}
+	// 	var matt = {
+	// 		location: "bay",
+	// 		age:5
+	// 	}
+
+	// $scope.objArr = [brian, nick, sam, matt];
+
 	
-	$scope.dogs = ['frenchie', 'corgie', 'rot', 'pit'];
-
-});
+	
