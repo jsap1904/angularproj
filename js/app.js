89881a5280e7b4cbb4944c3d653134b08111c4ba
jsap1904 <jsapdevs@gmail.com>
2017-07-30 11:24:54 -0700

    update

--- js/app.js
+++ js/app.js
@@ -2,5 +2,16 @@ var app = angular.module("myApp", []);
 
 app.controller('firstCtrl', function($scope) {
 	$scope.myName = 'Jet';
+
 	console.log($scope.myName);
+
+//change name function
+	$scope.logChanges= function(x) {
+		console.log(x);
+	};
+
+	$scope.nameArr = ['brian', 'nick', 'sam', 'matt'];
+	
+	$scope.dogs = ['frenchie', 'corgie', 'rot', 'pit'];
+
 });
